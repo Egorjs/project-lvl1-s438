c348ae062eeb5997e00498ecd12437d5a3762e28
Egorjs <mail_to_egr@mail.ru>
2019-02-06 02:28:28 +0300

    project step 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import * as readlineSync from '../node_modules/readline-sync';
 
 const askName = () => {
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
-}
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
 export default askName;
