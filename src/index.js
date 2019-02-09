01bcdb42c12613bbd42c7d59da6b9daa89feb5ff
Egorjs <mail_to_egr@mail.ru>
2019-02-09 12:18:10 +0300

    fix remarks step 4

--- src/index.js
+++ src/index.js
@@ -10,18 +10,25 @@ const askName = () => {
   return userName;
 };
 
+const isEven = (number) => {
+  const result = number % 2 === 0 ? 'yes' : 'no';
+  return result;
+};
+
 const logic = () => {
   welcome();
   const userName = askName();
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.round(Math.random() * (100 - 1 + 1) + 1);
-    const answer = readlineSync.question(`Question: ${number} `);
+  const rounds = 3;
+  for (let i = 0; i < rounds; i += 1) {
+    const question = Math.round(Math.random() * (100 - 1 + 1) + 1);
+    const answer = readlineSync.question(`Question: ${question} `);
     console.log(`Your answer: ${answer}`);
-    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+    const correctAnswer = isEven(question);
     if (answer === correctAnswer) {
       console.log('Correct!');
     } else {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\r\nLet's try again, ${userName}`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      return console.log(`Let's try again, ${userName}`);
     }
   }
   return console.log(`Congratulations, ${userName}`);
